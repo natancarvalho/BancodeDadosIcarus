d1e67ced129de5c64e33c1677362901d0d7cd3b4
natancarvalho <natancarvalho@poli.ufrj.br>
2020-10-08 22:11:16 -0300

    Update conectar.php

--- src/funcoes/conectar.php
+++ src/funcoes/conectar.php
@@ -1,11 +1,11 @@
 <?php
  /* Dados do Banco de Dados a conectar */
-$Servidor = "34.95.209.115";
+$Servidor = "";
 $nomeBanco = "dados_treino_setup";
 $nomeBanco2 = "dados_estrutural";
 $nomeBanco3 = "dados_usuarios";
 $Usuario = "root";
-$Senha = "C@rvalhoBD";
+$Senha = "";
 $strcon = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco);
 $strcon2 = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco2); 
 $strcon3 = mysqli_connect($Servidor, $Usuario, $Senha, $nomeBanco3);
