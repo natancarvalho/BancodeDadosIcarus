cac7a66b6cc0f8a6c6ee47ccc9bc7e01c3998ca7
U-CEPEL\0362973 <0362973@DT-FU-020292.cepel.br>
2026-09-11 14:11:13 -0300

    Teste Semgrep

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <link rel="icon" href="images/cropped-cropped-Logo-escudo-192x192.png" sizes="192x192" />
     <link rel="apple-touch-icon" href="images/cropped-cropped-Logo-escudo-180x180.png" />
     <meta name="msapplication-TileImage" content="../../images/cropped-cropped-Logo-escudo-270x270.png" />
-    <title> Banco de Dados Icarus </title>
+    <title> Banco de Dados Icarus Teste Semgrep</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
